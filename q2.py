b9c46f96128d4d5ac55e306fb4277295d92a5930
yl22011 <92855412+yl22011@users.noreply.github.com>
2022-04-04 14:07:25 +0800

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,4 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+xy2=((x+y)/2)
+print(str(xy2))
---
+++ q3.py
@@ -0,0 +1,12 @@
+
+x = str(input("Enter module: "))
+match x:
+    case "CS1009":
+        print("Object Oriented Programming")
+    case "CS1006":
+        print("Math")
+    case "CS1007":
+        print("Operating Systems")
+
+for y in range(101,65,-2):
+    print(y)
